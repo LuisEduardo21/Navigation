3a89a27bdcebfdf6a209f8d87549b98e5bea9566
luis eduardo <lfdasilva9@gmail.com>
2021-03-18 21:53:07 -0300

    navegação usando tabs

--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import TextoCentral from '../components/TextoCentral'
+import React from 'react';
+import TextoCentral from '../components/TextoCentral';
 
 export default props => {
-    const route = props.route || {params: {numero : 0}}
-    return ( 
-        <TextoCentral corFundo = '#9932cd'>
-            Tela C - {route.params.numero}
-        </TextoCentral>
-    )
-}
+  const r = props.route
+  const numero =
+    r && r.params && r.params.numero
+      ? props.route.params.numero
+      : 0
+  return (
+    <TextoCentral corFundo="#9932cd">
+      Tela C - {numero}
+    </TextoCentral>
+  );
+};
